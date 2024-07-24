12e7ddadbbd9818f51921b3f2fa437440106fe2b
yash tiwari <149458186+ctrly4sh@users.noreply.github.com>
2024-07-25 00:12:39 +0530

    routing

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,15 @@
 import React from "react"
-import { BrowserRouter as Router , Routes , Route } from "react-router-dom"
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
 import './App.css'
-
+import { Signup } from "./components/Signup"
+import { Login } from "./components/Login"
 
 function App() {
   return <Router>
     <Routes>
-      <Route path="/" element={}/>
+      <Route path="/" element={<Signup />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/dashboard" element={<Login />} />
     </Routes>
   </Router>
 
