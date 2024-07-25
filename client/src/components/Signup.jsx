e81113c4870c8b4adedb5133cb488fe804b0666e
yash tiwari <149458186+ctrly4sh@users.noreply.github.com>
2024-07-25 21:19:36 +0530

    Update Signup.jsx

--- client/src/components/Signup.jsx
+++ client/src/components/Signup.jsx
@@ -1,9 +1,39 @@
 import React from "react"
+import { Link } from "react-router-dom"
 
 export const Signup = () => {
     return (
         <>
             Signup...
+
+            <form action="">
+                <p>
+
+                    <label htmlFor="">Name : </label>
+                    <input type="text" />
+
+                </p>
+
+                <p>
+
+                    <label htmlFor="">Email : </label>
+                    <input type="email" name="" id="" required />
+
+                </p>
+
+                <p>
+
+                    <label htmlFor="">Password : </label>
+                    <input type="password" name="" id="" />
+                </p>
+
+                <p>
+                    <Link to='/login'>
+                    <button type="submit">Signup</button>
+                    </Link>
+                </p>
+            </form>
+
         </>
     )
 }
